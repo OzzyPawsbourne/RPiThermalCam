79f55182416af945daa7b02a9bb85f1f0d0d1790
OzzyPawsbourne <75728379+OzzyPawsbourne@users.noreply.github.com>
2021-06-18 23:57:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,11 @@ from pylepton import Lepton
 import numpy as np
 import cv2
 
-CM = rpi_temp()
-
 with Lepton() as l:
     data,_ = l.capture()
 
 point = (0,0)
-thermal_image,temp_str = CM.process(data,point=point,palette_switch=True)
+thermal_image,temp_str = rpi_temp(data,point=point,palette_switch=True)
 
 print("Temperature at {} is {}".format(point,temp_str))
 cv2.imwrite("thermal_img.png",thermal_image)
-
